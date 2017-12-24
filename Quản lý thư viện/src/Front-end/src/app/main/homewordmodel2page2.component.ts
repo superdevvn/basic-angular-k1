62f30d21d5d5bfaadcce92b3de4298a38f5d8744
Nguyễn Công Sơn <nguyencongson163@gmail.com>
2017-12-24 10:52:18 +0700

    no message

---
+++ Quản lý thư viện/src/Front-end/src/app/main/homewordmodel2page2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+selector: 'model2-page2',
+templateUrl: './homewordmodel2page2.component.html'
+})
+export class Homewordmodel2page2Component {
+}
---
+++ Quản lý thư viện/src/Front-end/src/app/main/homework.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+selector: 'homework',
+templateUrl: './homework.component.html'
+})
+export class HomewordComponent {
+}
