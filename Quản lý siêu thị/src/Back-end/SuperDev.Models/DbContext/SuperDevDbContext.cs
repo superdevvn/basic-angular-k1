ccf64f7dfafff59053d0877496f7775aabf87442
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2017-12-24 10:35:25 +0700

    - Init Back-end QLST

---
+++ Quản lý siêu thị/src/Back-end/SuperDev.Models/DbContext/SuperDevDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SuperDev.Models
+{
+    public partial class SuperDevDbContext : DbContext
+    {
+        public SuperDevDbContext()
+            : base("Default")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer<SuperDevDbContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public int SaveChanges(int? id = null)
+        {
+            return base.SaveChanges();
+        }
+    }
+}
