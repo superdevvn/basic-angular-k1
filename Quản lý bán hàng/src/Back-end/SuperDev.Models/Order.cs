a8111181e33f164042576db5f3cacd041316d1f6
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2017-12-24 10:33:34 +0700

    - Init back-end QLBH

---
+++ Quản lý bán hàng/src/Back-end/SuperDev.Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SuperDev.Models
+{
+    public partial class SuperDevDbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime? DeliveryDate { get; set; }
+
+        public bool IsPayed { get; set; }
+
+        public string Description { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
