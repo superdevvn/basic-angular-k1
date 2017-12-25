f4481bddf8979705e61dca93d5ad6b28d8312b52
Nguyễn Công Sơn <nguyencongson163@gmail.com>
2017-12-25 13:41:14 +0700

    gửi Thảo tiếp tục list-detail

---
+++ Menu/src/app/main/homewordmodel1page2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+selector: 'model1-page2',
+templateUrl: './homewordmodel1page2.component.html'
+})
+export class Homewordmodel1page2Component {
+}
---
+++ Menu/src/app/main/role-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'role-list',
+    templateUrl:'./role-list.component.html'
+})
+export class RoleListComponent {
+    ngAfterViewInit(){
+    }
+}
