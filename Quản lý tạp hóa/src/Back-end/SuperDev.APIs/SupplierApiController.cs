035af1d689516dc3e58b1c986ce596de4b5d84c8
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2017-12-29 00:08:22 +0700

    - Init api for Role and Supplier

---
+++ Quản lý tạp hóa/src/Back-end/SuperDev.APIs/SupplierApiController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using SuperDev.Models;
+using SuperDev.Services;
+
+namespace SuperDev.APIs
+{
+    public partial class SuperDevApiController : ApiController
+    {
+        [HttpGet]
+        [Route("api/getSupplier/{id}")]
+        public IHttpActionResult GetSupplier(int id)
+        {
+            var supplierService = new SupplierService();
+            return Ok(supplierService.GetById(id));
+        }
+
+        [HttpPost]
+        [Route("api/saveSupplier")]
+        public IHttpActionResult SaveSupplier([FromBody]Supplier supplier)
+        {
+            var supplierService = new SupplierService();
+            return Ok(supplierService.PersistSupplier(supplier));
+        }
+
+        [HttpPost]
+        [Route("api/getSuppliers")]
+        public IHttpActionResult GetSuppliers()
+        {
+            var supplierService = new SupplierService();
+            return Ok(supplierService.GetList());
+        }
+    }
+}
--- Quản lý tạp hóa/src/Back-end/SuperDev.Services/RoleService.cs
+++ Quản lý tạp hóa/src/Back-end/SuperDev.Services/RoleService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using SuperDev.Models;
 using SuperDev.Repositories;
 
@@ -15,6 +11,18 @@ namespace SuperDev.Services
             var roleRepository = new RoleRepository();
             if (role.Id > 0) return roleRepository.Update(role);
             return roleRepository.Create(role);
+        }
+
+        public IEnumerable<Role> GetList()
+        {
+            var roleRepository = new RoleRepository();
+            return roleRepository.GetEntities();
+        }
+
+        public Role GetById(int id)
+        {
+            var roleRepository = new RoleRepository();
+            return roleRepository.GetEntity(id);
         }
     }
 }
