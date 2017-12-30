e35e815cb43584743de04ac3230c60615bc7cba3
Chí Trung <trung.ktpm@gmail.com>
2017-12-30 09:46:35 +0700

    chí trung nè

---
+++ Quản lý siêu thị/src/Font-end/src/src/app/Sale/order-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'order-list',
+    templateUrl:'./fish-list.component.html'
+})
+export class FishListComponent {
+    ngAfterViewInit(){
+    }
+}
---
+++ Quản lý siêu thị/src/Font-end/src/src/app/customers/gold-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gold-card',
+    templateUrl:'./gold-card.component.html'
+})
+export class GoldCardComponent {
+    ngAfterViewInit(){
+    }
+}
