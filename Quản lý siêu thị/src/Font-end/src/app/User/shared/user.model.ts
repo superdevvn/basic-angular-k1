8b4ad28e42b27f24c9ae1dc132a6981e0c826859
Chí Trung <trung.ktpm@gmail.com>
2017-12-31 10:44:30 +0700

    <createdDate>

--- Quản lý siêu thị/src/Font-end/src/app/User/shared/user.model.ts
+++ Quản lý siêu thị/src/Font-end/src/app/User/shared/user.model.ts
@@ -7,7 +7,9 @@ export class User {
     LastName: string;
     Description: string;
     IsActived: string;
-    CreateDate: string;
+    CreatedDate: Date;
     CreateBy: string;
-    constructor() {}
+    constructor() {
+        this.CreatedDate= new Date();
+    }
 }
