95dc9bc87d0d7daeb9e6aca87f97779c4c329f17
Chí Trung <trung.ktpm@gmail.com>
2017-12-31 10:21:13 +0700

    <sửa>

---
+++ Quản lý siêu thị/src/Font-end/src/app/Unit/service/unit.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Role } from '../shared/unit.model';
+
+@Injectable()
+export class RoleService {
+    constructor(private apiService: ApiService) { }
+    saveRole(role: Role) {
+        return new Promise((resolve, reject) => {
+            this.apiService.post('api/saveRole', role).then((res:Response) => {
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+   
+
+    getList() {
+        return new Promise((resolve, reject) => {
+            this.apiService.post('api/getRoles',{}).then((res:Response) => {
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    getRole(id) {
+        return new Promise((resolve, reject) => {
+            this.apiService.get(`api/getRole/${id}`).then((res:Response) => {
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+    deleteRole(role: Role[]) {
+        return new Promise((resolve, reject) => {
+            this.apiService.post('api/deleteRole', role).then((res:Response) => {
+                resolve(res.json());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+}
---
+++ Quản lý siêu thị/src/Font-end/src/app/Unit/unit-list.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoleService } from './../Role/service/role.service';
+import { Role } from './../Role/shared/role.model';
+
+@Component({
+    selector: 'role-list',
+    templateUrl:'./role-list.component.html'
+})
+export class RoleListComponent {
+
+    roles:Role[] = [];
+
+    constructor(private router: Router, private roleService: RoleService) {
+    }
+
+    ngOnInit(){
+        this.roleService.getList().then((res:Role[])=>{
+            this.roles = res;
+            console.log("B");
+        }).catch(err=>{
+            alert(err);
+        });
+
+        console.log("A");
+    }
+
+    ngAfterViewInit(){
+    }
+
+    detail(role:Role){
+        this.router.navigate(['/main/role-detail',role.Id]);
+    }
+    // delete(id: number) {
+    //    const index= this.roles.findIndex(role => role.Id === id)
+    //         this.roles.slice(index,1);
+    //         alert("delete Success");
+
+    // }
+    delete(id: number) {
+        this.roleService.deleteRole(this.roles).then((res: Role[]) => {
+            //Server trả về role sau khi save
+            //Nếu là tạo role mới thì res sẽ có giá trị id mới thay vì 0
+            this.roles = res;
+            const index= this.roles.findIndex(role => role.Id === id)
+            this.roles.slice(index,1);
+            alert("delete Success");
+        }).catch(err => {
+            alert(err);
+        })
+    }
+}
