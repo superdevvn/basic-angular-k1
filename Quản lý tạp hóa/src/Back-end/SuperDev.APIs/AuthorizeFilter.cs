6d71c92576c0ca0f2114343def6df230d1e50613
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-01-06 10:55:11 +0700

    - Fix back-end

--- Quản lý tạp hóa/src/Back-end/SuperDev.APIs/AuthorizeFilter.cs
+++ Quản lý tạp hóa/src/Back-end/SuperDev.APIs/AuthorizeFilter.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using SuperDev.Services;
 
 namespace SuperDev.APIs
 {
@@ -13,16 +11,18 @@ namespace SuperDev.APIs
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            //var a = actionContext.Request.Headers
-            //var b = a;
-        }
+            try
+            {
+                //var token = actionContext.Request.Headers.FirstOrDefault(header => header.Key == "Auth-SuperDev").Value.ToString();
+                var userService = new UserService();
+                if (userService.GetCurrentUser() != null)
+                    base.OnActionExecuting(actionContext);
+            }
+            catch
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+            }
 
-        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
-        {
-            //if(actionExecutedContext.Request.Properties["Authorization"] != null)
-            //{
-            //    actionExecutedContext.Response.Content.Headers.Add("auth-superdev", actionExecutedContext.Request.Properties["Authorization"].ToString());
-            //}
         }
     }
 }
